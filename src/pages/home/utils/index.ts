6d74472e451a2cc837595df372b775788c418f77
cqc <tuomodaji0726@163.com>
2022-08-14 09:56:02 +0800

    feat: toggle login action to class

--- src/pages/home/utils/index.ts
+++ src/pages/home/utils/index.ts
@@ -1,143 +1,168 @@
 import { useGlobalStore, useUserStore, useMessageStore } from '@/stores';
 import api from '@/service/api';
-import global from '@/config/global';
-// import { } from
+
+enum LoginType {
+	WeChat,
+	AliPay
+}
+
+abstract class LoginHandler {
+	abstract handler(payload?: any);
+}
+
+class GStores {
+	messageStore = useMessageStore();
+	userStore = useUserStore();
+	globalStore = useGlobalStore();
+}
+
+class LoginUtils extends GStores {
+	async getUerInfo() {
+		const { result } = await api.userInfoByToken({});
+
+		if (result) {
+			const { cellPhoneNum, herenId, idNo, name, sex } = result;
+
+			this.userStore.updateName(name);
+			this.userStore.updateSex(sex);
+			this.userStore.updateIdNo(idNo);
+			this.userStore.updatePhone(cellPhoneNum);
+
+			this.globalStore.setHerenId(herenId);
+
+			if (!herenId) {
+				this.messageStore.showMessage('未完善，请先完善', 1000);
+				setTimeout(() => {
+					uni.reLaunch({
+						url: '/pagesA/medicalCardMan/addMedical'
+					});
+				}, 1200);
+			}
+		}
+	}
+
+	outLogin() {
+		this.userStore.clearStore();
+		this.globalStore.clearStore();
+
+		setTimeout(() => {
+			this.messageStore.showMessage('退出成功', 1500);
+		}, 500);
+	}
+}
+
+class WeChatLoginHandler extends LoginUtils implements LoginHandler {
+	handler(payload?: any) {
+		// 微信 必然有 payload
+		if (!payload) return;
+		const { target, detail } = payload;
+
+		if (detail.errMsg !== 'getPhoneNumber:ok') {
+			this.messageStore.showMessage('用户取消授权', 1500);
+			return;
+		}
+
+		wx.login({
+			success: async ({ code }) => {
+				const accountType = this.globalStore.browser.accountType;
+
+				const { result } = await api.getAppletsOpenId({
+					code,
+					accountType
+				});
+
+				if (result) {
+					const { openId, sessionKey } = result;
+					const {
+						encryptedData: encrypData,
+						iv: ivData,
+						code: phoneNumberCode
+					} = target;
+
+					this.globalStore.setOpenId(openId);
+
+					const requestData = {
+						accountType,
+						openId,
+						sessionKey,
+						phoneNumberCode,
+						ivData,
+						encrypData
+					};
+
+					const { result: loginResult } =
+						await api.wxLoginByPhoneNumberCode(requestData);
+
+					if (loginResult) {
+						const { accessToken, refreshToken } = loginResult;
+						this.globalStore.setToken({
+							accessToken,
+							refreshToken
+						});
+
+						this.getUerInfo();
+					}
+				}
+			}
+		});
+	}
+}
+
+class AliPayLoginHandler extends LoginUtils implements LoginHandler {
+	async handler() {
+		my.getAuthCode({
+			scopes: 'auth_user',
+			success: async ({ authCode }) => {
+				const accountType = this.globalStore.browser.accountType;
+				const { result } = await api.getTPAlipayUserInfoShare({
+					code: authCode,
+					codeType: 2,
+					accountType
+				});
+
+				const { userId, accessToken, refreshToken, authHerenId } =
+					result;
+
+				if (accountType === 1) {
+					this.globalStore.setH5OpenId(userId);
+				} else {
+					this.globalStore.setOpenId(userId);
+				}
+
+				this.globalStore.setToken({
+					accessToken,
+					refreshToken
+				});
+
+				this.getUerInfo();
+			},
+
+			fail: ({ errorMessage }) => {
+				this.messageStore.showMessage(errorMessage);
+			}
+		});
+	}
+}
+
+class Login {
+	public static handlerMap: Record<LoginType, LoginHandler> = {
+		[LoginType.WeChat]: new WeChatLoginHandler(),
+		[LoginType.AliPay]: new AliPayLoginHandler()
+	};
+
+	static handler(type: LoginType, payload?: any) {
+		Login.handlerMap[type].handler(payload);
+	}
+}
 
 export const aliLogin = function () {
-	my.getAuthCode({
-		scopes: 'auth_user',
-		success({ authCode }) {
-			aliInLogin({
-				code: authCode,
-				codeType: 2
-			});
-		},
-
-		fail({ errorMessage }) {
-			const messageStore = useMessageStore();
-			messageStore.showMessage(errorMessage);
-		}
-	});
+	Login.handler(LoginType.AliPay);
 };
 
 export const wxLogin = function (e) {
-	const messageStore = useMessageStore();
-	const globalStore = useGlobalStore();
-	const accountType = globalStore.browser.accountType;
-
-	const { target, detail } = e;
-
-	if (detail.errMsg !== 'getPhoneNumber:ok') {
-		messageStore.showMessage('用户取消授权', 1500);
-		return;
-	}
-
-	wx.login({
-		success: async ({ code }) => {
-			const { result } = await api.getAppletsOpenId({
-				code,
-				accountType
-			});
-
-			if (result) {
-				const { openId, sessionKey } = result;
-				const {
-					encryptedData: encrypData,
-					iv: ivData,
-					code: phoneNumberCode
-				} = target;
-
-				globalStore.setOpenId(openId);
-
-				const requestData = {
-					accountType,
-					openId,
-					sessionKey,
-					phoneNumberCode,
-					ivData,
-					encrypData
-				};
-
-				const { result: loginResult } =
-					await api.wxLoginByPhoneNumberCode(requestData);
-
-				if (loginResult) {
-					const { accessToken, refreshToken } = loginResult;
-					globalStore.setToken({
-						accessToken,
-						refreshToken
-					});
-
-					getUerInfo();
-				}
-			}
-		}
-	});
-};
-
-const aliInLogin = async function ({ codeType, code }) {
-	const globalStore = useGlobalStore();
-	const accountType = globalStore.browser.accountType;
-
-	const { result } = await api.getTPAlipayUserInfoShare({
-		code,
-		codeType,
-		accountType
-	});
-
-	const { userId, accessToken, refreshToken, authHerenId } = result;
-
-	if (accountType === 1) {
-		globalStore.setH5OpenId(userId);
-	} else {
-		globalStore.setOpenId(userId);
-	}
-
-	globalStore.setToken({
-		accessToken,
-		refreshToken
-	});
-
-	getUerInfo();
-};
-
-export const getUerInfo = async function () {
-	const { result } = await api.userInfoByToken({});
-
-	if (result) {
-		const userStore = useUserStore();
-		const globalStore = useGlobalStore();
-		const messageStore = useMessageStore();
-
-		const { cellPhoneNum, herenId, idNo, name, sex } = result;
-
-		userStore.updateName(name);
-		userStore.updateSex(sex);
-		userStore.updateIdNo(idNo);
-		userStore.updatePhone(cellPhoneNum);
-
-		globalStore.setHerenId(herenId);
-
-		if (!herenId) {
-			messageStore.showMessage('未完善，请先完善', 1000);
-			setTimeout(() => {
-				uni.reLaunch({
-					url: '/pagesA/medicalCardMan/addMedical'
-				});
-			}, 1200);
-		}
-	}
+	Login.handler(LoginType.WeChat, e);
 };
 
 export const outLogin = function () {
-	const messageStore = useMessageStore();
-	const userStore = useUserStore();
-	const globalStore = useGlobalStore();
-
-	userStore.clearStore();
-	globalStore.clearStore();
-
-	setTimeout(() => {
-		messageStore.showMessage('退出成功', 1500);
-	}, 500);
+	new LoginUtils().outLogin();
 };
