67487d79bdc7dcc46aa6523f03b2bbc0131272f7
cqc <tuomodaji0726@163.com>
2022-09-06 14:34:39 +0800

    n

--- src/pagesA/medicalCardMan/addMedical.vue
+++ src/pagesA/medicalCardMan/addMedical.vue
@@ -1,124 +1,112 @@
 <template>
-    <view>
-        <scroll-view class="scroll-container" scroll-y>
-            <view class="form-container">
-                <g-form
-                    v-model:value="formData"
-                    @submit="formSubmit"
-                    @change="formChange"
-                    @input-blur="formInputBlur"
-                    @select-change="selectChange"
-                    @address-change="addressChange"
-                    @ocr-ident="ocrIdent"
-                    :show-require-icon="false"
-                    bodyBold
-                    ref="gform"
-                />
-            </view>
+  <view>
+    <scroll-view class="scroll-container" scroll-y>
+      <view class="form-container">
+        <g-form
+          v-model:value="formData"
+          @submit="formSubmit"
+          @change="formChange"
+          @input-blur="formInputBlur"
+          @select-change="selectChange"
+          @address-change="addressChange"
+          @ocr-ident="ocrIdent"
+          :show-require-icon="false"
+          bodyBold
+          ref="gform"
+        />
+      </view>
 
-            <g-message />
-            <view class="footer">
-                <button
-                    @click="gform.submit"
-                    :class="{
-                        'btn-disabled': btnDisabled
-                    }"
-                    class="btn btn-primary"
-                >
-                    保存
-                </button>
-            </view>
-        </scroll-view>
-    </view>
+      <g-message />
+      <view class="footer">
+        <button
+          @click="gform.submit"
+          :class="{
+            'btn-disabled': btnDisabled
+          }"
+          class="btn btn-primary"
+        >
+          保存
+        </button>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
 <script lang="ts" setup>
-import { ref, nextTick, onMounted, computed } from 'vue';
-import { FormKey, pickTempItem, formKey, TFormKeys } from './utils';
-import {
-    GStores,
-    idValidator,
-    PatientUtils,
-    routerJump,
-    OcrRes
-} from '@/utils';
-import { onReady } from '@dcloudio/uni-app';
+  import { ref, nextTick, onMounted, computed } from 'vue';
+  import { FormKey, pickTempItem, formKey, TFormKeys } from './utils';
+  import { GStores, idValidator, PatientUtils, ServerStaticData, routerJump, OcrRes } from '@/utils';
+  import { onReady } from '@dcloudio/uni-app';
 
-import dayjs from 'dayjs';
+  import dayjs from 'dayjs';
 
-const props = defineProps<{
+  const props = defineProps<{
     patientName: 'string';
     medicalType: 'string';
     verifyCode: 'string';
     defaultFalg: 'string';
     patientPhone: 'string';
     pageType: 'addPatient' | 'perfectReal';
-}>();
-const patientUtils = new PatientUtils();
-const gStores = new GStores();
-const gform = ref<any>('');
-const formData = ref<Partial<Record<TFormKeys, any>>>({});
-const addressChoose = {
+  }>();
+  const patientUtils = new PatientUtils();
+  const gStores = new GStores();
+  const gform = ref<any>('');
+  const formData = ref<Partial<Record<TFormKeys, any>>>({});
+  const addressChoose = {
     addressProvince: '',
     addressCity: '',
     addressCounty: '',
     addressCountyCode: ''
-};
-let verifyCode = '';
+  };
+  let verifyCode = '';
 
-let formList = pickTempItem([
-    'medicalType',
-    'patientName',
-    'patientPhone',
-    'verify',
-    'defaultFalg'
-]);
+  let formList = pickTempItem(['medicalType', 'patientName', 'patientPhone', 'verify', 'defaultFalg']);
 
-const formSubmit = async ({ data }) => {
+  const formSubmit = async ({ data }) => {
     const formKeyNow = formList.map((o) => o.key);
     const filterData = Object.fromEntries(
-        Object.entries(data).map(([key, value]) => {
-            return [key, formKeyNow.includes(key) ? value : undefined];
-        })
+      Object.entries(data).map(([key, value]) => {
+        return [key, formKeyNow.includes(key) ? value : undefined];
+      })
     );
 
     const requestData = {
-        ...filterData,
-        ...addressChoose,
-        verifyCode
+      ...filterData,
+      ...addressChoose,
+      verifyCode
     };
 
     if (props.pageType === 'perfectReal') {
-        await patientUtils.registerUser(requestData, {
-            addPatInterface: 'relevantPatient'
-        });
+      await patientUtils.registerUser(requestData, {
+        addPatInterface: 'relevantPatient'
+      });
     } else {
-        await patientUtils.addRelevantPatient(requestData);
+      await patientUtils.addRelevantPatient(requestData);
     }
 
     routerJump('/pagesA/medicalCardMan/medicalCardMan');
-};
+  };
 
-const formChange = ({ item, value, oldValue }) => {
+  const formChange = ({ item, value, oldValue }) => {
     if (item.key === formKey.medicalType && oldValue !== value) {
-        medicalTypeChange(value);
+      medicalTypeChange(value);
     }
-};
+  };
 
-const selectChange = (e) => {
+  const selectChange = (e) => {
     const { item, value } = e;
 
     switch (item.key) {
-        case formKey.idType:
-            idCardChange();
-            break;
+      case formKey.idType:
+        idCardChange();
+        break;
 
-        default:
-            break;
+      default:
+        break;
     }
-};
+  };
 
-const addressChange = (e) => {
+  const addressChange = (e) => {
     const { value } = e;
     const [addressProvince, addressCity, addressCounty] = value;
 
@@ -126,196 +114,207 @@ const addressChange = (e) => {
     addressChoose.addressCity = addressCity.text;
     addressChoose.addressCounty = addressCounty.text;
     addressChoose.addressCountyCode = addressCounty.value;
-};
+  };
 
-// 证件类型变化
-const idCardChange = () => {
+  // 证件类型变化
+  const idCardChange = () => {
     formData.value[formKey.idCard] = '';
 
     nextTick(() => {
-        medicalTypeChange(formData.value[formKey.medicalType]);
+      medicalTypeChange(formData.value[formKey.medicalType]);
     });
-};
+  };
 
-const formInputBlur = (e) => {
+  const formInputBlur = (e) => {
     const { item, value } = e;
 
-    if (
-        item.key == formKey.idCard &&
-        formData.value[formKey.medicalType] === '-1'
-    ) {
-        medicalTypeChange('-1');
+    if (item.key == formKey.idCard && formData.value[formKey.medicalType] === '-1') {
+      medicalTypeChange('-1');
     }
-};
+  };
 
-const ocrIdent = (res: OcrRes) => {
-    console.log('ocr', res);
-    const {} = res;
-};
+  const ocrIdent = async (res: OcrRes) => {
+    const { name, nation, address, idCard } = res;
 
-/**
- *
- * @param value
- * 	-1  成人、儿童（有证件）
- * 	0  新生儿（无证件)
- *  1  军人
- *  2  军属
- */
-const medicalTypeChange = async (value: '-1' | '0' | '1' | '2') => {
+    if (address) {
+      formData.value.location = address;
+    }
+
+    if (name) {
+      formData.value.patientName = name;
+    }
+
+    if (idCard) {
+      formData.value.idCard = idCard;
+    }
+
+    if (nation) {
+      const list = await ServerStaticData.getNationTerms();
+      const item = list.find((o) => o.label === nation);
+      formData.value.nation = item?.value || nation;
+    }
+  };
+
+  /**
+   *
+   * @param value
+   * 	-1  成人、儿童（有证件）
+   * 	0  新生儿（无证件)
+   *  1  军人
+   *  2  军属
+   */
+  const medicalTypeChange = async (value: '-1' | '0' | '1' | '2') => {
     const listArr: TFormKeys[] = [formKey.medicalType];
     const _sexAndBirth = [formKey.sex, formKey.birthday];
     const _parentInfo = [formKey.upName, formKey.upIdCard];
 
     switch (value) {
-        case '-1':
-            let lessThenSix: boolean = false;
+      case '-1':
+        let lessThenSix: boolean = false;
 
-            // 证件类型： 身份证
-            if (formData.value[formKey.idType] === '01') {
-                const idCard = formData.value[formKey.idCard];
+        // 证件类型： 身份证
+        if (formData.value[formKey.idType] === '01') {
+          const idCard = formData.value[formKey.idCard];
 
-                // 有身份证不需要填写 生日、性别
-                _sexAndBirth.length = 0;
+          // 有身份证不需要填写 生日、性别
+          _sexAndBirth.length = 0;
 
-                if (idCard && idValidator.checkIdCardNo(idCard)) {
-                    const cardInfo = idValidator.getIdCardInfo(idCard);
-                    if (cardInfo.age <= 6) {
-                        lessThenSix = true;
-                    }
-                }
-            } else {
-                const birthday = formData.value[formKey.birthday] as string;
-
-                if (birthday) {
-                    lessThenSix = dayjs().diff(dayjs(birthday), 'year') <= 6;
-                }
+          if (idCard && idValidator.checkIdCardNo(idCard)) {
+            const cardInfo = idValidator.getIdCardInfo(idCard);
+            if (cardInfo.age <= 6) {
+              lessThenSix = true;
             }
+          }
+        } else {
+          const birthday = formData.value[formKey.birthday] as string;
 
-            // 显示监护人
-            if (!lessThenSix) {
-                _parentInfo.length = 0;
-            }
+          if (birthday) {
+            lessThenSix = dayjs().diff(dayjs(birthday), 'year') <= 6;
+          }
+        }
 
-            listArr.push(
-                ...[
-                    formKey.patientName,
-                    ..._sexAndBirth,
-                    formKey.idType,
-                    formKey.idCard,
-                    ..._parentInfo,
-                    formKey.nation,
-                    formKey.patientPhone,
-                    formKey.address,
-                    formKey.location,
-                    formKey.defaultFalg
-                ]
-            );
+        // 显示监护人
+        if (!lessThenSix) {
+          _parentInfo.length = 0;
+        }
 
-            break;
+        listArr.push(
+          ...[
+            formKey.patientName,
+            ..._sexAndBirth,
+            formKey.idType,
+            formKey.idCard,
+            ..._parentInfo,
+            formKey.nation,
+            formKey.patientPhone,
+            formKey.address,
+            formKey.location,
+            formKey.defaultFalg
+          ]
+        );
 
-        case '0':
-            listArr.push(
-                ...[
-                    formKey.patientName,
-                    ..._sexAndBirth,
-                    ..._parentInfo,
-                    formKey.nation,
-                    formKey.patientPhone,
-                    formKey.address,
-                    formKey.location,
-                    formKey.defaultFalg
-                ]
-            );
-            break;
+        break;
 
-        default:
-            gStores.messageStore.showMessage('未知的就诊人类型');
-            break;
+      case '0':
+        listArr.push(
+          ...[
+            formKey.patientName,
+            ..._sexAndBirth,
+            ..._parentInfo,
+            formKey.nation,
+            formKey.patientPhone,
+            formKey.address,
+            formKey.location,
+            formKey.defaultFalg
+          ]
+        );
+        break;
+
+      default:
+        gStores.messageStore.showMessage('未知的就诊人类型');
+        break;
     }
 
     formList = pickTempItem(listArr);
 
     const idCardItem = formList.find((o) => o.key === formKey.idCard);
     if (idCardItem) {
-        idCardItem.validator = (v) => {
-            const value = v as string;
-            let isErr = false;
-            if (formData.value[formKey.idType] === '01') {
-                if (!idValidator.checkIdCardNo(value)) {
-                    isErr = true;
-                }
-            }
-
-            if (isErr) {
-                return Promise.resolve({
-                    success: false,
-                    message: '请确认证件号码是否有误'
-                });
-            } else {
-                return Promise.resolve({
-                    success: true
-                });
-            }
-        };
-
-        const patientNameItem = formList.find(
-            (o) => o.key === formKey.patientName
-        );
-
-        if (patientNameItem) {
-            patientNameItem.ocr = true;
-            // patientNameItem.showSuffixArrowIcon = true;
+      idCardItem.validator = (v) => {
+        const value = v as string;
+        let isErr = false;
+        if (formData.value[formKey.idType] === '01') {
+          if (!idValidator.checkIdCardNo(value)) {
+            isErr = true;
+          }
         }
+
+        if (isErr) {
+          return Promise.resolve({
+            success: false,
+            message: '请确认证件号码是否有误'
+          });
+        } else {
+          return Promise.resolve({
+            success: true
+          });
+        }
+      };
+
+      const patientNameItem = formList.find((o) => o.key === formKey.patientName);
+
+      if (patientNameItem) {
+        patientNameItem.ocr = true;
+        // patientNameItem.showSuffixArrowIcon = true;
+      }
+    }
+
+    const patientNameItem = formList.find((o) => o.key === formKey.patientName);
+    if (patientNameItem) {
+      patientNameItem.disabled = true;
     }
 
     formList.map((o) => {
-        const { key } = o;
-        if (
-            [
-                formKey.medicalType,
-                formKey.patientName,
-                formKey.patientPhone
-            ].includes(key as any)
-        ) {
-            if (formData.value[key]) {
-                o.disabled = true;
-            }
+      const { key } = o;
+      if ([formKey.medicalType, formKey.patientName, formKey.patientPhone].includes(key as any)) {
+        if (formData.value[key]) {
+          o.disabled = true;
         }
+      }
     });
 
     console.log(formList);
 
     gform.value.setList(formList);
-};
+  };
 
-const btnDisabled = computed(() => {
+  const btnDisabled = computed(() => {
     let isDisabled = false;
     const formKeys = formList.map((o) => o.key);
     Object.entries(formData.value).map(([key, value]) => {
-        if (formKeys.includes(key) && value === '') {
-            isDisabled = true;
-        }
+      if (formKeys.includes(key) && value === '') {
+        isDisabled = true;
+      }
     });
 
     return isDisabled;
-});
+  });
 
-onReady(() => {
+  onReady(() => {
     if (props.pageType === 'perfectReal') {
-        uni.setNavigationBarTitle({
-            title: '完善账号实名信息'
-        });
+      uni.setNavigationBarTitle({
+        title: '完善账号实名信息'
+      });
     }
-});
+  });
 
-onMounted(() => {
+  onMounted(() => {
     formData.value = Object.fromEntries(
-        Object.entries(props).map(([key, value]) => {
-            if (key === formKey.defaultFalg) {
-                (value as any) = (value as unknown) === 'false' ? false : true;
-            }
-            return [key, value];
-        })
+      Object.entries(props).map(([key, value]) => {
+        if (key === formKey.defaultFalg) {
+          (value as any) = (value as unknown) === 'false' ? false : true;
+        }
+        return [key, value];
+      })
     );
 
     // 默认身份证
@@ -323,28 +322,28 @@ onMounted(() => {
     verifyCode = formData.value[formKey.verify];
 
     nextTick(() => {
-        // medicalTypeChange('-1');
-        medicalTypeChange(formData.value[formKey.medicalType]);
+      // medicalTypeChange('-1');
+      medicalTypeChange(formData.value[formKey.medicalType]);
     });
-});
+  });
 </script>
 
 <style lang="scss" scoped>
-.scroll-container {
+  .scroll-container {
     height: 100vh;
 
     .form-container {
-        // margin: 16rpx 32rpx;
-        margin-bottom: 100rpx;
+      // margin: 16rpx 32rpx;
+      margin-bottom: 100rpx;
     }
-}
+  }
 
-.footer {
+  .footer {
     position: fixed;
     bottom: 0;
     right: 0;
     left: 0;
     background-color: var(--h-color-white);
     padding: 32rpx 32rpx 68rpx;
-}
+  }
 </style>
--- src/utils/modules/ocr.ts
+++ src/utils/modules/ocr.ts
@@ -2,228 +2,216 @@ import { useMessageStore } from '@/stores/modules/globalMessage';
 import { useGlobalStore } from '@/stores/modules/global';
 import api from '@/service/api';
 
-type GreaterThan<
-	T extends number,
-	U extends number,
-	R extends any[] = []
-> = T extends R['length']
-	? false
-	: U extends R['length']
-	? true
-	: GreaterThan<T, U, [...R, 1]>;
+type GreaterThan<T extends number, U extends number, R extends any[] = []> = T extends R['length']
+  ? false
+  : U extends R['length']
+  ? true
+  : GreaterThan<T, U, [...R, 1]>;
 
 type TChooseImgBase64Res =
-	| {
-			success: true;
-			base64: string;
-	  }
-	| {
-			success: false;
-			evt: any;
-	  };
+  | {
+      success: true;
+      base64: string;
+    }
+  | {
+      success: false;
+      evt: any;
+    };
 
 const wxDownFileToLocal = function (url: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		wx.downloadFile({
-			url,
-			success(e) {
-				const { tempFilePath } = e;
-				resolve(tempFilePath);
-			},
+  return new Promise((resolve, reject) => {
+    wx.downloadFile({
+      url,
+      success(e) {
+        const { tempFilePath } = e;
+        resolve(tempFilePath);
+      },
 
-			fail(e) {
-				reject(e);
-			}
-		});
-	});
+      fail(e) {
+        reject(e);
+      }
+    });
+  });
 };
 
-const getWeChatBase64ImageByUrl = function (
-	url: string
-): Promise<TChooseImgBase64Res> {
-	return new Promise(async (r, j) => {
-		// 确认是本地是还是网络的url
-		if (url.startsWith('http')) {
-			const _url = await wxDownFileToLocal(url);
+const getWeChatBase64ImageByUrl = function (url: string): Promise<TChooseImgBase64Res> {
+  return new Promise(async (r, j) => {
+    // 确认是本地是还是网络的url
+    if (url.startsWith('http')) {
+      const _url = await wxDownFileToLocal(url);
 
-			if (_url) {
-				url = _url;
-			}
-		}
+      if (_url) {
+        url = _url;
+      }
+    }
 
-		wx.getFileSystemManager().readFile({
-			filePath: url, //选择图片返回的相对路径
-			encoding: 'base64', //编码格式
-			success: ({ data }) => {
-				r({
-					success: true,
-					base64: data
-					// base64: 'data:image/jpg;base64,' + data
-				});
-			},
-			fail: (evt) => {
-				j({
-					success: false,
-					evt
-				});
-			}
-		});
-	});
+    wx.getFileSystemManager().readFile({
+      filePath: url, //选择图片返回的相对路径
+      encoding: 'base64', //编码格式
+      success: ({ data }) => {
+        r({
+          success: true,
+          base64: data
+          // base64: 'data:image/jpg;base64,' + data
+        });
+      },
+      fail: (evt) => {
+        j({
+          success: false,
+          evt
+        });
+      }
+    });
+  });
 };
 
-const checkFileSize = ({
-	filePath,
-	fileSize
-}: {
-	filePath: string;
-	fileSize: number;
-}) => {
-	const messageStore = useMessageStore();
+const checkFileSize = ({ filePath, fileSize }: { filePath: string; fileSize: number }) => {
+  const messageStore = useMessageStore();
 
-	return new Promise((resolve, reject) => {
-		uni.getFileInfo({
-			filePath,
-			fail: reject,
-			complete: (res) => {
-				const { size } = res;
+  return new Promise((resolve, reject) => {
+    uni.getFileInfo({
+      filePath,
+      fail: reject,
+      complete: (res) => {
+        const { size } = res;
 
-				if (size < fileSize) {
-					resolve(res);
-				} else {
-					// showMessage('请选择小于 2M 的相片');
-					const str = `请选择小于 ${(fileSize / 1024 / 1024).toFixed(
-						1
-					)}M 的相片`;
-					messageStore.showMessage(str);
-					reject(str);
-				}
-			}
-		});
-	});
+        if (size < fileSize) {
+          resolve(res);
+        } else {
+          // showMessage('请选择小于 2M 的相片');
+          const str = `请选择小于 ${(fileSize / 1024 / 1024).toFixed(1)}M 的相片`;
+          messageStore.showMessage(str);
+          reject(str);
+        }
+      }
+    });
+  });
 };
 
 // 获取 base64
 export const chooseImg = (
-	payload: Partial<{
-		extension: ('jpg' | 'jpeg' | 'image')[];
-		fileSize?: number;
-	}> = {}
+  payload: Partial<{
+    extension: ('jpg' | 'jpeg' | 'image')[];
+    fileSize?: number;
+  }> = {}
 ): Promise<TChooseImgBase64Res> => {
-	const { extension, fileSize } = payload;
+  const { extension, fileSize } = payload;
 
-	return new Promise((resolve, reject) => {
-		uni.chooseImage({
-			extension,
-			count: 1,
-			sizeType: 'compressed',
-			async success(e) {
-				const { tempFilePaths, tempFiles } = e;
+  return new Promise((resolve, reject) => {
+    uni.chooseImage({
+      extension,
+      count: 1,
+      sizeType: 'compressed',
+      async success(e) {
+        const { tempFilePaths, tempFiles } = e;
 
-				if (tempFilePaths.length) {
-					if (fileSize) {
-						await checkFileSize({
-							filePath: tempFilePaths[0],
-							fileSize
-						});
-					}
+        if (tempFilePaths.length) {
+          if (fileSize) {
+            await checkFileSize({
+              filePath: tempFilePaths[0],
+              fileSize
+            });
+          }
 
-					// #ifdef MP-WEIXIN
-					return resolve(
-						await getWeChatBase64ImageByUrl(tempFilePaths[0])
-					);
-					// #endif
+          // #ifdef MP-WEIXIN
+          return resolve(await getWeChatBase64ImageByUrl(tempFilePaths[0]));
+          // #endif
 
-					reject({
-						success: false,
-						evt: '当前只有微信'
-					});
-				} else {
-					reject({
-						success: false,
-						evt: '获取图片失败'
-					});
-				}
-			},
+          reject({
+            success: false,
+            evt: '当前只有微信'
+          });
+        } else {
+          reject({
+            success: false,
+            evt: '获取图片失败'
+          });
+        }
+      },
 
-			fail() {}
-		});
-	});
+      fail() {}
+    });
+  });
 };
 
 const ocrForWX = async () => {
-	const globalStore = useGlobalStore();
-	const e = await chooseImg({
-		extension: ['image', 'jpeg', 'jpg'],
-		fileSize: 2 * 1024 * 1024
-	});
+  const globalStore = useGlobalStore();
+  const e = await chooseImg({
+    extension: ['image', 'jpeg', 'jpg'],
+    fileSize: 2 * 1024 * 1024
+  });
 
-	if (e.success) {
-		const requestData = {
-			source: globalStore.browser.source,
-			fileName: 'cssc',
-			base64: e.base64
-		};
+  if (e.success) {
+    const requestData = {
+      source: globalStore.browser.source,
+      fileName: 'cssc',
+      base64: e.base64
+    };
 
-		await api.ocrIdCard(requestData);
-	} else {
-		throw new Error(e.evt);
-	}
+    await api.ocrIdCard(requestData);
+  } else {
+    throw new Error(e.evt);
+  }
 };
 
 const findSuccess = ({ name, sex, nation, birth, idCard, address }) => {
-	if (nation) {
-		if (!nation.includes('族')) {
-			nation += '族';
-		}
-	}
+  if (nation) {
+    if (!nation.includes('族')) {
+      nation += '族';
+    }
+  }
 
-	return {
-		name,
-		sex,
-		nation,
-		birth,
-		idCard,
-		address
-	};
+  return {
+    name,
+    sex,
+    nation,
+    birth,
+    idCard,
+    address
+  };
 };
 
 const ocrForAlipay = async () => {
-	const messageStore = useMessageStore();
+  const messageStore = useMessageStore();
 
-	const { readIDCard, IDCardTypes } = requirePlugin('ocrPlugin');
-	const bizId = 'emblem';
+  const { readIDCard, IDCardTypes } = requirePlugin('ocrPlugin');
+  const bizId = 'emblem';
 
-	const res = await readIDCard({ bizId, type: IDCardTypes.FRONT });
-	console.log(res);
-	const { error, data } = res;
+  const res = await readIDCard({ bizId, type: IDCardTypes.FRONT });
+  const { error, data } = res;
 
-	if (error) {
-		messageStore.showMessage(error.errorMessage, 1500);
-		return Promise.reject(error);
-	} else {
-		const { name, sex, nationality, birth, num: idCard, address } = data;
+  if (error) {
+    const msg = error.errorMessage;
+    if (msg && typeof msg === 'string') {
+      messageStore.showMessage(error.errorMessage, 1500);
+    }
 
-		return findSuccess({
-			name: name.data,
-			sex: sex.data,
-			nation: nationality.data,
-			birth: birth.data,
-			idCard: idCard.data,
-			address: address.data
-		});
-	}
+    return Promise.reject(error);
+  } else {
+    const { name, sex, nationality, birth, num: idCard, address } = data;
+
+    return findSuccess({
+      name: name.data,
+      sex: sex.data,
+      nation: nationality.data,
+      birth: birth.data,
+      idCard: idCard.data,
+      address: address.data
+    });
+  }
 };
 
-export const useOcr = async () => {
-	const messageStore = useMessageStore();
+type OcrFindRes = ReturnType<typeof findSuccess>;
+export type OcrRes = Record<keyof OcrFindRes, string>;
 
-	// #ifdef MP-WEIXIN
-	await ocrForWX();
-	// #endif
+export const useOcr = async (): Promise<OcrRes> => {
+  const messageStore = useMessageStore();
 
-	// #ifdef MP-ALIPAY
-	return await ocrForAlipay();
-	// #endif
+  // #ifdef MP-WEIXIN
+  await ocrForWX();
+  // #endif
+
+  // #ifdef MP-ALIPAY
+  return await ocrForAlipay();
+  // #endif
 };
-
-export type OcrRes = ReturnType<typeof findSuccess>;
