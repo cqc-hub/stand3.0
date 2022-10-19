b9c25f83cbf2e287ce55cbb05acdcb7047197568
cqc <tuomodaji0726@163.com>
2022-10-19 22:24:19 +0800

    feat: ts-utils

--- src/typeUtils/index.ts
+++ src/typeUtils/index.ts
@@ -1,1 +1,7 @@
 export type Flatten<T> = { [K in keyof T]: T[K] };
+
+type Without<T, U> = {
+  [P in Exclude<keyof T, keyof U>]?: never;
+};
+
+export type XOR<T, U> = (Without<T, U> & U) | (Without<U, T> & T);
