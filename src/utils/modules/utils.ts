e2fbd0398b983760ea0f00a703adb8761f1a3087
dudandan <dudandan@herenit.com>
2022-11-17 17:30:31 +0800

    修复bug 新增渭南市中心医院

--- src/utils/modules/utils.ts
+++ src/utils/modules/utils.ts
@@ -78,7 +78,7 @@ export const routerJump = async (url?: `/${string}`) => {
       const menuItem = getMenuById(routerStore._id, menus);
 
       if (menuItem) {
-        useCommonTo(menuItem, { type: 'reLaunch' });
+        useCommonTo(menuItem);
       } else {
         // messageStore.showMessage(
         //   '未找到对应menuId 的 menu：' + routerStore._id
