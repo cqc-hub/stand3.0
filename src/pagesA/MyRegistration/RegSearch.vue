d2bb74112ee55fcc0f4b9e6e750f85a9ae60c427
cqc <tuomodaji0726@163.com>
2022-11-25 14:16:51 +0800

    search

---
+++ src/pagesA/MyRegistration/RegSearch.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="g-page">
+    <view class="search-header">
+      <view class="search-input">
+        <uni-search-input
+          v-model:value="searchText"
+          placeholder="请输入医生/科室/症状"
+        />
+      </view>
+    </view>
+
+    <view class="g-container">233</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+  import { defineComponent, ref } from 'vue';
+
+  const searchText = ref('');
+</script>
+
+<style lang="scss" scoped>
+  .search-input {
+    margin: 16rpx 32rpx;
+  }
+
+  .search-header {
+    background-color: #fff;
+  }
+</style>
