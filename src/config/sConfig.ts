395485fba2613290eb9df1a573f26030d3979ec6
dudandan <dudandan@herenit.com>
2023-01-11 16:34:59 +0800

    新增配置参数 是否隐藏搜索框 底部新增互联网医院

--- src/config/sConfig.ts
+++ src/config/sConfig.ts
@@ -1,6 +1,10 @@
 interface ISConfig {
   // 有药品配送功能?
   isDrugDelivery?: '1';
+  //底部是否放开互联网医院?
+  isOpenButtom?: Object; 
+  //首页是否隐藏搜索框?
+  isHideHomeSearch?: '1';
 }
 
 const scJson: {
@@ -12,6 +16,12 @@ const scJson: {
 
   1001054: {
     isDrugDelivery: '1',
+    isOpenButtom:{
+      appId:'wxdaadb2180235cdcc',
+      path:'/pages/index/index'
+    },
+    isHideHomeSearch:'1'
+
   },
 };
 
