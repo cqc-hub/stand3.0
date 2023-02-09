c50b473dea711d4ecb43a857a046792628932ffd
cqc <tuomodaji0726@163.com>
2023-02-09 10:21:50 +0800

    1

--- src/pages/home/componetns/homeTabbar.vue
+++ src/pages/home/componetns/homeTabbar.vue
@@ -129,7 +129,7 @@
         height: 100%;
 
         .label {
-          font-size: var(--h-size-40);
+          font-size: var(--hr-font-size-xxxs);
           color: var(--hr-neutral-color-8);
           margin-top: 10rpx;
         }
