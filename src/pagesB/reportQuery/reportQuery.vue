3af1e55ac839cace3afc4c829c1e49340e0256c2
cqc <chenqinchuan@herenit.com>
2023-08-16 16:22:28 +0800

    1

--- src/pagesB/reportQuery/reportQuery.vue
+++ src/pagesB/reportQuery/reportQuery.vue
@@ -408,7 +408,7 @@
 
           useTBanner(yunBannerConfig.value);
         } else {
-          gStores.messageStore.showMessage('未获取到云影像数据');
+          gStores.messageStore.showMessage('未获取到云影像数据', 3000);
         }
       }
     }
