f1b1ac7d8df480dd9df3545a73d21795ecad7a39
cqc <chenqinchuan@herenit.com>
2023-08-17 17:10:29 +0800

    build 去除 console

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,13 @@ export default defineConfig({
       },
     },
   },
+
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
 });
