aaf2e654213e9cf27bbbd742638386a88eeb8875
cqc <chenqinchuan@herenit.com>
2023-08-18 10:26:59 +0800

    1

--- src/pagesA/medicalCardMan/addMedical.vue
+++ src/pagesA/medicalCardMan/addMedical.vue
@@ -722,11 +722,11 @@
     // #endif
     // }
 
-    formData.value.patientName = '陈钦川';
-    formData.value.idCard = '330326199908286713';
-    formData.value.location = 'jjjjjjj';
-    formData.value.patientPhone = '15797812958';
-    formData.value.nation = '01';
+    // formData.value.patientName = '陈钦川';
+    // formData.value.idCard = '330326199908286713';
+    // formData.value.location = 'jjjjjjj';
+    // formData.value.patientPhone = '15797812958';
+    // formData.value.nation = '01';
 
     nextTick(() => {
       medicalTypeChange(formData.value[formKey.patientType]);
