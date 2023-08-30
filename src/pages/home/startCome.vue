4a84b49cae399dbc70244fbb515a42bb07ab70de
cqc <chenqinchuan@herenit.com>
2023-08-30 14:01:10 +0800

    1

--- src/pages/home/startCome.vue
+++ src/pages/home/startCome.vue
@@ -8,7 +8,7 @@
   import { defineComponent, ref } from 'vue';
   import global from '@/config/global';
 
-  const appId = global.systemInfo.h5Appid;
+  let appId = global.systemInfo.h5Appid;
   const isStartComeTest = global.systemInfo.isStartComeTest;
   const src = ref('');
 
@@ -23,23 +23,13 @@
     baseUrl = 'https://tzwwz.eheren.com';
   }
 
-  if (isStartComeTest) {
-    src.value =
-      (global.env as string) === 'prod'
-        ? `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(
-            baseUrl + '/h5/index.html'
-          )}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
-        : `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxac2942d44e7c2bd9&redirect_uri=${encodeURIComponent(
-            baseUrl + '/h5/index.html'
-          )}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`;
-  } else {
-    src.value = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(
-      baseUrl + '/h5/index.html'
-    )}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`;
-  }
+  isStartComeTest &&
+    (global.env as string) !== 'prod' &&
+    (appId = 'wxac2942d44e7c2bd9');
 
-  console.warn(src.value);
+  src.value = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(
+    baseUrl + '/h5/index.html'
+  )}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`;
 </script>
 
 <style lang="scss" scoped></style>
-https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx56d1a143ff27e903&redirect_uri=https%3A%2F%2Fhealth.eheren.com%2Fh5%2Findex.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect
