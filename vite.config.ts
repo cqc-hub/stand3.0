b305ce56165834e345199f21e1e77014c64e451c
cqc <chenqinchuan@herenit.com>
2023-10-17 10:59:52 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,21 @@ import uni from '@dcloudio/vite-plugin-uni';
 import { resolve } from 'path';
 import h5ProdEffectPlugin from 'uni-vite-plugin-h5-prod-effect';
 
+const os = require('os');
+const interfaces = os.networkInterfaces();
+let netIp = '';
+
+for (const devName in interfaces) {
+  const iface = interfaces[devName];
+
+  iface.map((o) => {
+    const { family, address, internal } = o;
+    if (family === 'IPv4' && address !== '127.0.0.1' && !internal) {
+      netIp = address;
+    }
+  });
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni(), h5ProdEffectPlugin()],
@@ -21,6 +36,11 @@ export default defineConfig({
         additionalData: `@import "@/styles/variable.scss";`,
       },
     },
+  },
+
+  server: {
+    open: true,
+    host: netIp,
   },
 
   // build: {
