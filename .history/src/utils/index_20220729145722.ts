cc0b2c696024ded1cfd7d40add303015c1597312
cqc <chenqinchuan@herenit.com>
2023-12-14 15:55:51 +0800

    update .gitignore

--- .history/src/utils/index_20220729145722.ts
+++
@@ -1,8 +0,0 @@
-import { encryptedAes, decrypt, useDecryByAes, encryptDes } from './des';
-
-export {
-  encryptedAes,
-  decrypt,
-  useDecryByAes,
-  encryptDes
-}
--- .history/src/utils/index_20220729150156.ts
+++
@@ -1,5 +0,0 @@
-import { encryptDes } from './des';
-
-export {
-  encryptDes
-}
