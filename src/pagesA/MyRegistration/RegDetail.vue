eae95bdec76d7160cc7dfce4f805d1e032390eb7
cqc <chenqinchuan@herenit.com>
2024-04-23 09:53:02 +0800

    1

--- src/pagesA/MyRegistration/RegDetail.vue
+++ src/pagesA/MyRegistration/RegDetail.vue
@@ -658,7 +658,15 @@
       hosInfo.value = hos;
     }
 
-    const { downTime, qrCode, totalCost, hosAccountOffsetFee } = result;
+    const {
+      downTime,
+      qrCode,
+      totalCost,
+      hosAccountOffsetFee,
+      appointmentDate,
+      ampmName,
+      appointmentTime,
+    } = result;
     if (downTime) {
       timeTravel.value.downTime = downTime;
       startTimeTravel();
@@ -672,9 +680,9 @@
       result._hosAccountOffsetFee = hosAccountOffsetFee + '元';
     }
 
-    result._appointmentDate = `${result.appointmentDate || ''} ${
-      (result.ampmName || '') + (result.appointmentTime || '')
-    }`;
+    result._appointmentDate = [appointmentDate, ampmName, appointmentTime]
+      .filter((o) => o)
+      .join(' ');
     result._fee = result.fee + '元';
     result._category = result.schQukCategor || result.categorName;
     orderRegInfo.value = result;
