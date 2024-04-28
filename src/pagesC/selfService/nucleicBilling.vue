f2437da94ab9f0434ed40fc4a3f4a7726b8f4fab
cqc <chenqinchuan@herenit.com>
2024-04-28 15:25:37 +0800

    1

--- src/pagesC/selfService/nucleicBilling.vue
+++ src/pagesC/selfService/nucleicBilling.vue
@@ -386,11 +386,11 @@
       return;
     }
     const idx = selList.value.findIndex((o) => o.itemCode === item.itemCode);
-    if (idx === -1 && tips) {
-      await new Promise((r: any) => {
+    if (idx === -1 && pageConfig.value.clickItemShowTipInDialog && tips) {
+      await new Promise((closeCallBack: any) => {
         tips &&
           gStores.messageStore.showMessage(tips, 3000, {
-            closeCallBack: r,
+            closeCallBack,
             useDialog: true,
           });
       });
