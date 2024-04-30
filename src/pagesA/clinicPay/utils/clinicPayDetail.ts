607ab5e6d333d6c0db191558c852ddf9982a2d3b
cqc <chenqinchuan@herenit.com>
2024-04-30 09:32:07 +0800

    医保经纬度限制

--- src/pagesA/clinicPay/utils/clinicPayDetail.ts
+++ src/pagesA/clinicPay/utils/clinicPayDetail.ts
@@ -412,17 +412,19 @@ export const _getQxMedicalNation = async (
   }
 
   // #ifdef MP-ALIPAY
-  const { latitude, longitude } = await apiAsync(uni.getLocation, {});
+  let { latitude, longitude } = await apiAsync(uni.getLocation, {});
+  if (!(longitude && latitude)) {
+    gStores.messageStore.showMessage('获取定位失败, 无法继续医保结算...');
+    return Promise.reject('获取定位失败, 无法继续医保结算...');
+  }
+
+  latitude = ((latitude * 1).toFixed(6) as unknown as number) * 1;
+  longitude = ((longitude * 1).toFixed(6) as unknown as number) * 1;
 
   result.userLongitudeLatitude = {
     latitude,
     longitude,
   };
-
-  if (!(longitude && latitude)) {
-    gStores.messageStore.showMessage('获取定位失败, 无法继续医保结算...');
-    return Promise.reject('获取定位失败, 无法继续医保结算...');
-  }
 
   // #endif
 
