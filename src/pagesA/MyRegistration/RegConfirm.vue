0d4c6fe58eb5602118b84f335b37d39a6b34b753
cqc <chenqinchuan@herenit.com>
2024-12-19 17:26:41 +0800

    feat: 按钮名称修改

--- src/pagesA/MyRegistration/RegConfirm.vue
+++ src/pagesA/MyRegistration/RegConfirm.vue
@@ -47,7 +47,7 @@
                 >
                   <text class="iconfont qr-toggle-icon">&#xe6c3;</text>
 
-                  快速预约
+                  无卡快速预约
                 </view>
               </view>
             </template>
