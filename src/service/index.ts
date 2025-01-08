edddd002afe6028709ebeb6c8b5033f47e3934b2
杜丹丹 <dudandan@herenit.com>
2025-01-08 09:48:16 +0800

    小程序默认配置改为des加密

--- src/service/index.ts
+++ src/service/index.ts
@@ -28,9 +28,11 @@ const globalStore = useGlobalStore();
 let outLoginTimer: number;
 
 //是否加密 正式环境默认开启sm4加密
-const isDes = false;
-const isOpenSm4 =
-  (globalGl.env as string) === 'prod' ? true : globalGl.isOpenSm4;
+// 20250108 由于sm4容易导致系统压力大 所以统一改为des加密
+const isDes =  (globalGl.env as string) === 'prod' ? true : globalGl.isOpenDes;
+// const isOpenSm4 = (globalGl.env as string) === 'prod' ? true : globalGl.isOpenSm4;
+const isOpenSm4 = false;
+
 const getShowUrl = (url, baseUrl) =>
   url.slice(baseUrl?.length || 0).split('=')[0];
 
