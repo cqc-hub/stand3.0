ea934c53221f1097a1c9bb02c35ab5eafb0d3b76
杜丹丹 <dudandan@herenit.com>
2025-01-15 13:29:06 +0800

    修改判断

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <view v-if="globalStore.isShowFlag&&globalStore.sysCode==='1001063'" class="g-page">
+  <view v-if="globalStore.sysCode==='1001063'?globalStore.isShowFlag:true" class="g-page">
     <home-Nav />
     <scroll-view
       @scroll="pageScroll"
