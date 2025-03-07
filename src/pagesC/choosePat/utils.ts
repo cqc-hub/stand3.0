dfc936be61c6699c4b2542fda86b76ab02da2b6a
cqc <chenqinchuan@herenit.com>
2025-03-07 16:49:31 +0800

    chore: update crypto-js to version 4.2.0 and clean up unused hooks and plugins

--- src/pagesC/choosePat/utils.ts
+++ src/pagesC/choosePat/utils.ts
@@ -1,10 +1,6 @@
 import api from '@/service/api';
 import { IPat } from '@/stores';
 import { apiAsync, cacheUtil, useTBanner } from '@/utils';
-import { ref, computed } from 'vue';
-import { encryptByTripleDES, encryptDes } from '@/common/des';
-import { joinQuery, joinQueryForUrl } from '@/common';
-import md5s from "js-md5";
 
 // 杭口
 export const HK_hook = () => {
@@ -76,45 +72,3 @@ export const HK_hook = () => {
 };
 
 // 宜兴
-export const YX_hook = () => {
-  const patClick = (pat: IPat) => {
-    console.log(pat);
-
-    const uid = `1559088022727692288`;
-    const appid = 'yxgj001';
-    const timestamp = Date.parse(new Date() as any) / 1000 - 95;
-    const appkey = `2c836cc79f22d391ff19bb12a5c50cd8`;
-    const signaturePart = `uid=${uid}&appid=${appid}&appkey=${appkey}&timestamp=${timestamp}`;
-    // @ts-expect-error
-    const signature = md5s(signaturePart);
-
-    // console.log(encryptDes('data', 'eWorldTomTaw#7=*', 'TomTaw#7'))
-    console.log(encryptByTripleDES('233', 'eWorldTomTaw#7=*', 'TomTaw#7'))
-    console.log(encryptByTripleDES('233', 'eWorldTomTaw#7=*', 'TomTaw#7'))
-    console.log(encryptByTripleDES('233', 'eWorldTomTaw#7=*', 'TomTaw#7'))
-
-
-    // return
-    const data = `{"med_rec_no":"${pat.cardNumber}","system_id":"1559108114005889024"}`;
-    const enCodeData = encodeURIComponent(
-      encryptByTripleDES(data, 'eWorldTomTaw#7=*', 'TomTaw#7')
-    );
-
-    console.log(enCodeData);
-    useTBanner({
-      type: 'h5',
-      path: joinQuery('https://pacs.yxph.com/ewcss-mobile/mobile/index', {
-        uid,
-        appid,
-        timestamp,
-        signature,
-        data: enCodeData,
-        callbackenv: 'miniapp',
-      }) as any,
-    });
-  };
-
-  return {
-    patClick,
-  };
-};
