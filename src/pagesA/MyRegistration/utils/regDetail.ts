a251ca8060c251d40b833fdf2e87cf0a661e1dcd
cqc <chenqinchuan@herenit.com>
2025-03-31 17:04:16 +0800

    fix: refactor order cancellation and refund logic for better clarity and flow

--- src/pagesA/MyRegistration/utils/regDetail.ts
+++ src/pagesA/MyRegistration/utils/regDetail.ts
@@ -526,7 +526,6 @@ export class RegDetailUtil {
       returnUrl: string;
     } = {} as any
   ) {
-    const { returnUrl } = opt;
     const { isOrderPay, wxOrderSubscribeMessage } = this.orderConfig.value;
     let errMsg = '';
 
@@ -545,24 +544,7 @@ export class RegDetailUtil {
     }
     // #endif
 
-    if (isOrderPay !== '1') {
-      const { confirm } = await new Promise<any>((closeCallBack) => {
-        this.gStores.messageStore.showMessage('', 0, {
-          useDialog: true,
-          dialogOpt: {
-            isShowCancel: true,
-            title: '确认取消该订单?',
-          },
-          closeCallBack,
-        });
-      });
-
-      if (!confirm) {
-        errMsg = '用户点击取消';
-        throw new Error(errMsg);
-      }
-      return await this.cancelReg(opt);
-    } else {
+    if (isOrderPay === '1') {
       const { refundNeedAuth, source, tradeType } = this.orderRegInfo;
       const { orderId } = this.prop.value;
       const args = {
@@ -580,12 +562,8 @@ export class RegDetailUtil {
       // #ifdef MP-WEIXIN
       isWx = true;
       // #endif
-      const yixinRefund =
-        this.gStores.globalStore.sysCode === '1001048' &&
-        tradeType === '2' &&
-        isWx;
 
-      if (refundNeedAuth === '0' && !yixinRefund) {
+      if (refundNeedAuth === '0') {
         if (isAlipay && source === 19) {
           errMsg = '本次挂号属于微信医保挂号, 暂不支持支付宝端退费';
           this.gStores.messageStore.showMessage(errMsg, 3000);
@@ -604,22 +582,21 @@ export class RegDetailUtil {
           'get-wx-medical-auth-code-order': '1',
         });
 
-        const authorize = await getQxMedicalNation({
-          returnUrl: joinQueryForUrl(
-            '/pagesA/MyRegistration/RegDetail',
-            this.prop.value
-          ),
-        });
+        if (this.gStores.globalStore.sysCode === '1001048') {
+          // 宜兴仅wx
+          args.payAuthNo = await getMedicalAuthCode();
+        } else {
+          const authorize = await getQxMedicalNation({
+            returnUrl: joinQueryForUrl(
+              '/pagesA/MyRegistration/RegDetail',
+              this.prop.value
+            ),
+          });
 
-        args.payAuthNo = authorize.payAuthNo;
+          args.payAuthNo = authorize.payAuthNo;
+        }
       }
 
-      if (yixinRefund && isWx) {
-        setLocalStorage({
-          'get-wx-medical-auth-code-order': '1',
-        });
-        args.payAuthNo = await getMedicalAuthCode();
-      }
       uni.showLoading({});
       const { title, content } = await this.gStores.getSysAppMore('1100');
       const { confirm } = await new Promise<any>((closeCallBack) => {
@@ -641,6 +618,23 @@ export class RegDetailUtil {
       console.log(args);
       // return
       await api.refundOrder(args);
+    } else {
+      const { confirm } = await new Promise<any>((closeCallBack) => {
+        this.gStores.messageStore.showMessage('', 0, {
+          useDialog: true,
+          dialogOpt: {
+            isShowCancel: true,
+            title: '确认取消该订单?',
+          },
+          closeCallBack,
+        });
+      });
+
+      if (!confirm) {
+        errMsg = '用户点击取消';
+        throw new Error(errMsg);
+      }
+      return await this.cancelReg(opt);
     }
   }
 
