6ae175335a4b6b8788e6d800d6041a750ebd987e
cqc <chenqinchuan@herenit.com>
2025-06-24 14:23:01 +0800

    feat: 3.0基线+嘉二】新增报告解读配置

--- src/types/modules/serverStaticData.ts
+++ src/types/modules/serverStaticData.ts
@@ -410,6 +410,8 @@ export interface ISystemConfig_ {
 
   //报告查询 REPORT_QUERY_CONFIG
   reportQuery: {
+    // 开启报告解读
+    reportAnalysis: '1';
     // 列表页开放选择时间
     isOpenFilterReportByTime?: '1';
     // 详情页开放收藏按钮
