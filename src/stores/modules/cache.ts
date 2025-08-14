e371e7da3337d7155cda32c5de16afc2eb11aeef
Sanshi3030 <1983683086@qq.com>
2025-08-14 18:22:03 +0800

    fix

--- src/stores/modules/cache.ts
+++ src/stores/modules/cache.ts
@@ -72,7 +72,7 @@ const cacheStore = defineStore('cache', {
           flag: '1212',
         },
         {
-          label: '隐私条款',
+          label: '隐私保护政策',
           flag: '1213',
         },
         {
