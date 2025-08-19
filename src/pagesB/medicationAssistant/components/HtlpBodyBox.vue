ccfd04cfe953a5e175e627fd0cd99f4419e149db
Sanshi3030 <1983683086@qq.com>
2025-08-19 09:41:50 +0800

    fix:门诊处方详情

--- src/pagesB/medicationAssistant/components/HtlpBodyBox.vue
+++ src/pagesB/medicationAssistant/components/HtlpBodyBox.vue
@@ -2,7 +2,7 @@
   <view class="item f32">
     <view class="g-bold f36 mb24">Rp</view>
 
-    <view v-if="isChineseMedical(item)">
+    <view v-if="isChineseMedical(item?.drugTypeName ? item : detailData)">
       <view class="g-bold g-break-word f28 mb16">
         <text v-if="detailData.num" class="mr12">{{ detailData.num }}剂,</text>
         <text v-if="detailData.amount" class="mr12">
@@ -15,7 +15,11 @@
       </view>
 
       <view v-if="detailData.drugDetailList" class="medical-content">
-        <view v-for="(citem, ci) in detailData.drugDetailList" :key="ci" class="medical-item f28 color-444">
+        <view
+          v-for="(citem, ci) in detailData.drugDetailList"
+          :key="ci"
+          class="medical-item f28 color-444"
+        >
           <text class="mr8">{{ citem.drugName }}</text>
           <text class="color-888">{{ citem.amount }}{{ citem.units }}</text>
         </view>
@@ -24,13 +28,20 @@
 
     <view v-else>
       <view v-if="detailData.drugDetailList" class="medical-content1">
-        <view v-for="(citem, ci) in detailData.drugDetailList" :key="ci" :class="{
-          'g-border-bottom': ci !== detailData.drugDetailList.length - 1,
-        }" class="medical-item1 f28 color-444 p24v">
+        <view
+          v-for="(citem, ci) in detailData.drugDetailList"
+          :key="ci"
+          :class="{
+            'g-border-bottom': ci !== detailData.drugDetailList.length - 1,
+          }"
+          class="medical-item1 f28 color-444 p24v"
+        >
           <view class="flex-between mb8">
             <view class="g-bold flex-start">
               <text>{{ citem.drugName }}</text>
-              <view class="medical-type" v-if="citem.isTake"><text>检查带药</text></view>
+              <view class="medical-type" v-if="citem.isTake">
+                <text>检查带药</text>
+              </view>
             </view>
             <view class="f28 color-888">
               x{{ citem.amount }}{{ citem.units }}
@@ -50,47 +61,48 @@
 </template>
 
 <script lang="ts" setup>
-import { defineComponent, ref } from 'vue';
-import {
-  type IItemDetail,
-  type IWaitListItem,
-  isChineseMedical,
-} from '../utils/medicalHelp';
+  import { defineComponent, ref } from 'vue';
+  import {
+    type IItemDetail,
+    type IWaitListItem,
+    isChineseMedical,
+  } from '../utils/medicalHelp';
 
-const props = defineProps<{
-  detailData: IItemDetail;
-  item: IWaitListItem;
-}>();
+  const props = defineProps<{
+    detailData: IItemDetail;
+    item: IWaitListItem;
+  }>();
 </script>
 
 <style lang="scss" scoped>
-.item {
-  padding: 40rpx 32rpx 24rpx;
-  background-color: #fff;
-  border-radius: 0px 0px 8px 8px;
+  .item {
+    padding: 40rpx 32rpx 24rpx;
+    background-color: #fff;
+    border-radius: 0px 0px 8px 8px;
 
-  .medical-content {
-    background: var(--hr-neutral-color-1);
-    border-radius: 4px;
-    padding: 16rpx 24rpx;
+    .medical-content {
+      background: var(--hr-neutral-color-1);
+      border-radius: 4px;
+      padding: 16rpx 24rpx;
 
-    display: grid;
-    grid-template: auto / repeat(2, 1fr);
+      display: grid;
+      grid-template: auto / repeat(2, 1fr);
 
-    .medical-item {
-      padding: 12rpx;
+      .medical-item {
+        padding: 12rpx;
+      }
+    }
+
+    .medical-content1 {
+    }
+
+    .medical-type {
+      font-weight: 400;
+      border-radius: 4px;
+      padding: 0 8rpx;
+      margin-left: 16rpx;
+      background-color: var(--hr-brand-color-1);
+      color: var(--hr-brand-color-6);
     }
   }
-
-  .medical-content1 {}
-
-  .medical-type {
-    font-weight: 400;
-    border-radius: 4px;
-    padding: 0 8rpx;
-    margin-left: 16rpx;
-    background-color: var(--hr-brand-color-1);
-    color: var(--hr-brand-color-6);
-  }
-}
 </style>
