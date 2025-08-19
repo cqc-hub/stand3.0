f320cbcd72c9b4557bfa2812588509f202e56665
cqc <chenqinchuan@herenit.com>
2025-08-19 18:59:26 +0800

    1

--- src/pagesA/MyRegistration/registrationType.vue
+++ src/pagesA/MyRegistration/registrationType.vue
@@ -95,12 +95,13 @@
           value: '2',
         },
         // 在线云门诊
-        // {
-        //   tip: '网络线上就诊',
-        //   img: `https://phsdevoss.eheren.com/pcloud/image/yylx_zxymz@2x.png`,
-        //   value: '',
-        //   path: '/pagesA/MyRegistration/registrationType?type=1',
-        // },
+        {
+          tip: '网络线上就诊',
+          img: `https://phsdevoss.eheren.com/pcloud/image/yylx_zxymz@2x.png`,
+          value: '',
+          path: '/pagesA/MyRegistration/registrationType',
+          // path: '/pagesA/MyRegistration/registrationType?type=1',
+        },
 
         // {
         //   tip: ' ',
