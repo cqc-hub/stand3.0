307e4802d55457d9eae36ceeacccc7823a4d2f9e
Sanshi3030 <1983683086@qq.com>
2025-08-22 11:16:57 +0800

    merge

--- src/utils/modules/utils.ts
+++ src/utils/modules/utils.ts
@@ -374,7 +374,7 @@ export const getLocation = async function (isForce?: boolean): Promise<{
           });
 
           await apiAsync(uni.openSetting, {});
-          isForce &&reAuth();
+          isForce && reAuth();
         }
 
         // #endif
