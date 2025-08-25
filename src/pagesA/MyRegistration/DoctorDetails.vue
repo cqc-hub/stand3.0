d6866c95e16267158d22d23dd11385847736f1aa
cqc <chenqinchuan@herenit.com>
2025-08-25 18:24:51 +0800

    feat: 医生名片添加评分

--- src/pagesA/MyRegistration/DoctorDetails.vue
+++ src/pagesA/MyRegistration/DoctorDetails.vue
@@ -373,7 +373,13 @@
         </view>
 
         <block v-if="isDocServiceShow">
-          <view class="f36 g-bold mb16 service-onlione p32c">在线服务</view>
+          <view class="mb16 mt56 p32c">
+            <text class="f36 g-bold mr24">在线服务</text>
+            <text v-if="docServiceInfo.satisfaction" class="f28">
+              <text class="mr12">评分</text>
+              <text class="color-warn">{{ docServiceInfo.satisfaction }}</text>
+            </text>
+          </view>
           <scroll-view class="service-content" scroll-x>
             <Doc-Service
               :docService="docServiceInfo"
@@ -1166,10 +1172,6 @@
         transparent 100%
       );
     }
-  }
-
-  .service-onlione {
-    margin-top: 56rpx;
   }
 
   .service-content {
