f8691ba7c0ee3062b6b849074337b037cf9c47df
cqc <chenqinchuan@herenit.com>
2025-11-07 16:56:28 +0800

    fix: update entry page path and clean up unused code

--- src/pagesA/MyRegistration/utils/index.ts
+++ src/pagesA/MyRegistration/utils/index.ts
@@ -723,47 +723,6 @@ export const useOrder = (props: Ref<IOrderProps>) => {
   const showWaitRegDialog = async (data: { scheme }) => {
     waitRegClickData.value = data;
     waitRegDialog.value.show();
-
-    // const config = await ServerStaticData.getSystemConfig('order');
-    // const { patientId } = gStores.userStore.patChoose;
-    // const {
-    //   ampm,
-    //   categor,
-    //   clinicalType,
-    //   hosDocId,
-    //   docName,
-    //   hosId,
-    //   schDate,
-    //   schId,
-    //   addedNum,
-    //   hosDeptId,
-    // } = data.scheme;
-    // const query = {
-    //   ampm,
-    //   categor,
-    //   clinicalType,
-    //   hosDocId,
-    //   docName,
-    //   hosId,
-    //   schDate,
-    //   schId,
-    //   addedNum: undefined,
-    //   patientId,
-    //   hosDeptId,
-    // };
-
-    // config.isOpenAddedNum === '1' && (query.addedNum = addedNum);
-
-    // const { result } = await api.canRegAlternate(query);
-    // if (result) {
-    //   waitRegClickData.value = data;
-    //   waitRegDialog.value.show();
-    // } else {
-    //   gStores.messageStore.showMessage(
-    //     '当前时段候补人数已达上限，暂不支持候补!',
-    //     3000
-    //   );
-    // }
   };
 
   return {
