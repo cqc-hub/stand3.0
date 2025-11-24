085b1a877f6be8b3d1d1378a7a308052ff231880
cqc <chenqinchuan@herenit.com>
2025-11-24 14:25:38 +0800

    feat: 表单组件优化

--- src/pagesA/medicalCardMan/utils/index.ts
+++ src/pagesA/medicalCardMan/utils/index.ts
@@ -165,6 +165,22 @@ export const tempList: TInstance[] = [
     placeholder: '请输入',
     key: formKey.idCard,
     labelWidth: '220rpx',
+    async validator(v, item, data) {
+      if (v) {
+        const { idType } = data;
+
+        if (idType === '01' && !idValidator.checkIdCardNo(v)) {
+          return Promise.resolve({
+            success: false,
+            message: '请确认证件号码是否有误',
+          });
+        }
+      }
+
+      return {
+        success: true,
+      };
+    },
   },
 
   {
