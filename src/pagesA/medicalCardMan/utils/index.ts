16493d80ee036b6a3720bf315646f3615e8da6f6
cqc <chenqinchuan@herenit.com>
2026-04-14 13:35:11 +0800

    chore: 设置标签宽度

--- src/pagesA/medicalCardMan/utils/index.ts
+++ src/pagesA/medicalCardMan/utils/index.ts
@@ -486,6 +486,12 @@ export const pickTempItem = function <T = TFormKeys>(
       return aIndex - bIndex;
     });
 
+  list.map((o) => {
+    if (o.field !== 'switch') {
+      o.labelWidth = '180rpx';
+    }
+  });
+
   if (sysCode === '1001067') {
     const referenceId = list.find((o) => o.key === 'referenceId');
     if (referenceId && referenceId.field === 'input-text') {
