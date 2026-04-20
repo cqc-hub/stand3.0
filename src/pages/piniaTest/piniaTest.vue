0e3a144a80d6093aac755cf5f381bd4fcbce2d9f
cqc <chenqinchuan@herenit.com>
2026-04-20 13:24:40 +0800

    chore: 调整拦截器提示逻辑

--- src/pages/piniaTest/piniaTest.vue
+++ src/pages/piniaTest/piniaTest.vue
@@ -767,11 +767,11 @@
     // useTBanner(用药查询1001093);
     // useTBanner(肿瘤住院管家);
     useTBanner({
-      _type: 'useTBanner',
-      _tip: '维护中',
-      _disabled: '1',
-      type: 'self',
+      type: 'h5',
+      isSelfH5: '1',
       path: '',
+      text: '',
+      _type: 'useTBanner',
     });
     // patientUtils.faceVerifyAndPData({
     //   name: '陈钦川',
