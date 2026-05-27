93653eb7e83b787eb21f992d1d405702c306b82a
cqc <chenqinchuan@herenit.com>
2026-05-27 17:09:28 +0800

    feat: 宜兴医保测试配置

--- proConfig.ts
+++ proConfig.ts
@@ -270,18 +270,6 @@ export const miniProgramConfig: {
             appId: 'wxe183cd55df4b4369',
             path: 'auth/pages/bindcard/auth/index',
             // cs
-            // pathExtraData: {
-            //   openType: 'getAuthCode',
-            //   cityCode: '320200',
-            //   channel: 'AAGDjhBtPzo4LJTh9gCenRkB',
-            //   orgChnlCrtfCodg:
-            //     'BqK1kMStlhVDgN2uHf4EsLK/F2LjZPYJ81nK2eYQqxvGdh09Ghvhyk/swHL2NBPe',
-            //   orgCodg: 'H32028200358',
-            //   bizType: '04107',
-            //   orgAppId: '1GDAN3EEO0123F60C80A000061B81AB7',
-            // },
-
-            // zs
             pathExtraData: {
               openType: 'getAuthCode',
               cityCode: '320200',
@@ -290,23 +278,23 @@ export const miniProgramConfig: {
                 'BqK1kMStlhVDgN2uHf4EsLK/F2LjZPYJ81nK2eYQqxvGdh09Ghvhyk/swHL2NBPe',
               orgCodg: 'H32028200358',
               bizType: '04107',
-              orgAppId: '1GU9S5QVB01M76430B0A000038F064B8',
+              orgAppId: '1GDAN3EEO0123F60C80A000061B81AB7',
             },
+
+            // zs
+            // pathExtraData: {
+            //   openType: 'getAuthCode',
+            //   cityCode: '320200',
+            //   channel: 'AAGDjhBtPzo4LJTh9gCenRkB',
+            //   orgChnlCrtfCodg:
+            //     'BqK1kMStlhVDgN2uHf4EsLK/F2LjZPYJ81nK2eYQqxvGdh09Ghvhyk/swHL2NBPe',
+            //   orgCodg: 'H32028200358',
+            //   bizType: '04107',
+            //   orgAppId: '1GU9S5QVB01M76430B0A000038F064B8',
+            // },
             pathExtraDataConfig: {
               // 虚拟id 互联网医院处方外配
               // zs
-              virtualHosId: {
-                openType: 'getAuthCode',
-                cityCode: '320200',
-                channel: 'AAFEEOdTYZNbEzD1gmhG9skn',
-                orgChnlCrtfCodg:
-                  'BqK1kMStlhVDgN2uHf4EsLK/F2LjZPYJ81nK2eYQqxvGdh09Ghvhyk/swHL2NBPe',
-                orgCodg: 'H32028200358',
-                bizType: '04104',
-                orgAppId: '1GU9S5QVB01M76430B0A000038F064B8',
-                sourceapp: 'wx8358079c24acea42-1',
-              },
-              // cs
               // virtualHosId: {
               //   openType: 'getAuthCode',
               //   cityCode: '320200',
@@ -318,14 +306,26 @@ export const miniProgramConfig: {
               //   orgAppId: '1GU9S5QVB01M76430B0A000038F064B8',
               //   sourceapp: 'wx8358079c24acea42-1',
               // },
+              // cs
+              virtualHosId: {
+                openType: 'getAuthCode',
+                cityCode: '320200',
+                channel: 'AAFEEOdTYZNbEzD1gmhG9skn',
+                orgChnlCrtfCodg:
+                  'BqK1kMStlhVDgN2uHf4EsLK/F2LjZPYJ81nK2eYQqxvGdh09Ghvhyk/swHL2NBPe',
+                orgCodg: 'H32028200358',
+                bizType: '04104',
+                orgAppId: '1GDAN3EEO0123F60C80A000061B81AB7',
+                sourceapp: 'wx8358079c24acea42-1',
+              },
             },
             dongRuanMedicalInfo: {
               h5BaseUrl:
                 // zs
-                'https://ybj.jszwfw.gov.cn/mms/hsa-tiap-ui/#/pay-loading',
+                // 'https://ybj.jszwfw.gov.cn/mms/hsa-tiap-ui/#/pay-loading',
 
               // cs
-              // 'https://ybj.jszwfw.gov.cn/mmc/hsa-mms-ui/#/pay-loading',
+              'https://ybj.jszwfw.gov.cn/mmc/hsa-mms-ui/#/pay-loading',
             },
           },
           isMedicalOrder: '1',
